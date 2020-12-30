8308a1650426c0cb2413df9fe78dc7e0da0fb1ed
gaowei1012 <mingwei2222@126.com>
2020-12-30 13:41:03 +0800

    add webpack

---
+++ webpack_cli/dist/main.bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function app() {
+    console.log('hello')
+}
+
+app()
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrX2NsaS8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBQUE7QUFDQTtBQUNBOztBQUVBLEsiLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBhcHAoKSB7XG4gICAgY29uc29sZS5sb2coJ2hlbGxvJylcbn1cblxuYXBwKCkiXSwic291cmNlUm9vdCI6IiJ9
