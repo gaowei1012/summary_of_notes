0911b9e428c58f456bd2aaa29917143b3bb907da
gaowei1012 <mingwei2222@126.com>
2021-01-01 22:56:45 +0800

    fix scss

--- vue-cli/config/webpack.base.js
+++ vue-cli/config/webpack.base.js
@@ -28,7 +28,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.scss$/,
+                test: /\.s[ac]ss$/,
                 use: [
                     "style-loader",
                     "css-loader",
