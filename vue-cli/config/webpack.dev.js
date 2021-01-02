f010cc1d5c130e1a253288b1509bb4bc9754fc5f
gaowei1012 <mingwei2222@126.com>
2021-01-02 10:50:54 +0800

    add webpack dev server

--- vue-cli/config/webpack.dev.js
+++ vue-cli/config/webpack.dev.js
@@ -10,6 +10,9 @@ module.exports = merge(base, {
     port: 1207, //端口
     open: false, //自动打开浏览器
     hot: true,
+    clientLogLevel: 'info',
+    historyApiFallback: true, // 使用HTML5历史记录API时，index.html可能必须在该页面上放置页面来代替任何404响应。devServer.historyApiFallback通过将其设置为启用true
+    // allowedHosts: {}, // 允许加入开发服务器
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
 });
