5945fd4b90bafcf3cbab8c28661908a8d176a2b2
gaowei1012 <mingwei2222@126.com>
2021-01-02 10:28:50 +0800

    add manifest plugin

--- vue-cli/config/webpack.prod.js
+++ vue-cli/config/webpack.prod.js
@@ -1,14 +1,18 @@
 const merge = require('webpack-merge');
 const base = require("./webpack.base.js");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const {WebpackManifestPlugin} = require('webpack-manifest-plugin')
 const path = require("path");
+const options = require('./options')
 
 module.exports = merge(base ,{
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(['dist'],{
             root: path.resolve(__dirname,'../'),
-        })
+        }),
+        /// 生成 manifest.json 文件，用于后面的 DllPlugin 代码拆分
+        new WebpackManifestPlugin(options)
     ],
     optimization: {
         runtimeChunk: 'single',
