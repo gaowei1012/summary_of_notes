f591b3873e94ae229d938dd654389df78fe41e38
gaowei1012 <gaomingwei2020@126.com>
2021-02-19 21:33:59 +0800

    add vue cli build

--- vue-cli/config/webpack.dev.js
+++ vue-cli/config/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(base, {
   devServer: {
     compress: true, //启用压缩
     port: 1207, //端口
-    open: false, //自动打开浏览器
+    open: true, //自动打开浏览器
     hot: true,
     clientLogLevel: 'info',
     historyApiFallback: true, // 使用HTML5历史记录API时，index.html可能必须在该页面上放置页面来代替任何404响应。devServer.historyApiFallback通过将其设置为启用true
